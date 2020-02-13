5de31bbf7ab73284ed7ffcffaf668cdb25c64732
uenoryo <uenoryo@uma-b.com>
2020-02-13 13:06:46 +0900

    pdfで保存できるように.

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -5,11 +5,16 @@
     </div>
     <div ref="result">
     </div>
+    <hr>
+    <div>
+      <button @click="save">保存</button>
+    </div>
   </div>
 </template>
 
 <script>
 import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
 
 export default {
   name: 'Sample',
@@ -18,6 +23,15 @@ export default {
     html2canvas(this.$refs.content).then((canvas) => {
       this.$refs.result.appendChild(canvas)
     })
+  },
+  methods: {
+    "save": function() {
+      let pdf = new jsPDF()
+      html2canvas(this.$refs.content).then((canvas) => {
+        pdf.addImage(canvas.toDataURL(), 'JPEG', 0, 0, 200, 0)
+        pdf.save('test.pdf')
+      })
+    },
   },
 }
 </script>
